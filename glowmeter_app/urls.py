eb152811cd0c002a0bba2d9f403a21e968cf4f50
TreesaKuriakose <kuriakosetreesa28@gmail.com>
2025-03-25 10:43:57 +0530

    fixed logout

--- glowmeter_app/urls.py
+++ glowmeter_app/urls.py
@@ -5,7 +5,10 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='registration/logged_out.html',
+        next_page='home'
+    ), name='logout'),
     path('register/', views.register_user, name='register'),
     path('register-doctor/', views.register_doctor, name='register_doctor'),
     path('dashboard/', views.dashboard, name='dashboard'),
